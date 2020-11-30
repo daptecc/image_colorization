8d42495a37258eab60204365777c3fb8055d91f8
Dan <dannykpark@gmail.com>
2020-11-30 04:58:48 +0000

    add option to train with pretrained generator

--- train_model.py
+++ train_model.py
@@ -1,10 +1,11 @@
 import glob, time, argparse
-from utils import create_loss_meters, update_losses, visualize, log_results
-from main_model import MainModel
-from colorization_dataset import make_dataloaders
 from tqdm.notebook import tqdm
 import numpy as np
 from fastai.data.external import untar_data, URLs
+from main_model import MainModel
+from colorization_dataset import make_dataloaders
+from funet import build_res_unet
+from utils import create_loss_meters, update_losses, visualize, log_results
 
 def train_model(model, train_dl, epochs, display_every=200, visualize_dir):
     data = next(iter(val_dl)) # getting a batch for visualizing the model output after fixed intervals
@@ -24,8 +25,9 @@ def train_model(model, train_dl, epochs, display_every=200, visualize_dir):
 
 
 if __name__ == '__main__':
-#     ap = argparse.ArgumentParser()
+     ap = argparse.ArgumentParser()
 #     ap.add_argument('-i', '--path', required=False, help='path to image dir')
+     ap.add_argument('-g', '--generator', required=False, help='path to pretrained generator')
 #     args = vars(ap.parse_args())
 
     coco_path = untar_data(URLs.COCO_SAMPLE)
@@ -43,7 +45,11 @@ if __name__ == '__main__':
     train_dl = make_dataloaders(paths=train_paths, split='train')
     val_dl = make_dataloaders(paths=val_paths, split='val')
     
-    model = MainModel()
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    net_G = build_res_unet(n_input=1, n_output=2, size=256)
+    net_G.load_state_dict(torch.load(args['generator'], map_location=device))
+        
+    model = MainModel(net_G=net_G)
     
     samples_dir = f'samples_{time.time()}'
     os.mkdir(samples_dir)
