bbf9de480fdd24c1a0c4eb83a9a04e07bc6bad86
Dan <dannykpark@gmail.com>
2020-12-02 19:16:07 +0000

    add args for pretrained geneerator; update README

--- README.md
+++ README.md
@@ -6,6 +6,12 @@ Adapted from [https://github.com/moein-shariatnia/Deep-Learning/tree/main/Image%
 
 Sampled 10000 COCO images for training
 
-Sample output after training:
+Sample output after 100 epochs of adversarial training with baseline generator:
 
 ![baseline sample](images/colorization_1606592237.921929.png "Baseline sample")
+
+#### Pretrained generator
+
+Sample output after 20 epochs of adversarial training with pretrained generator:
+
+![pretrained sample](images/pretrained_colorization_1606935432.3185506.png "Pretrained sample")
--- train_model.py
+++ train_model.py
@@ -1,13 +1,14 @@
-import glob, time, argparse
+import os, glob, datetime, argparse
 from tqdm.notebook import tqdm
 import numpy as np
+import torch
 from fastai.data.external import untar_data, URLs
 from main_model import MainModel
 from colorization_dataset import make_dataloaders
 from funet import build_res_unet
 from utils import create_loss_meters, update_losses, visualize, log_results
 
-def train_model(model, train_dl, epochs, display_every=200, visualize_dir):
+def train_model(model, train_dl, epochs, display_every=200, visualize_dir='samples'):
     data = next(iter(val_dl)) # getting a batch for visualizing the model output after fixed intervals
     for e in range(epochs):
         loss_meter_dict = create_loss_meters() # function returing a dictionary of objects to 
@@ -21,14 +22,15 @@ def train_model(model, train_dl, epochs, display_every=200, visualize_dir):
                 print(f"\nEpoch {e+1}/{epochs}")
                 print(f"Iteration {i}/{len(train_dl)}")
                 log_results(loss_meter_dict) # function to print out the losses
-                visualize(model, data, save=True, visualize_dir) # function displaying the model's outputs
+                visualize(model, data, save=True, outdir=visualize_dir) # function displaying the model's outputs
 
 
 if __name__ == '__main__':
-     ap = argparse.ArgumentParser()
+    ap = argparse.ArgumentParser()
 #     ap.add_argument('-i', '--path', required=False, help='path to image dir')
-     ap.add_argument('-g', '--generator', required=False, help='path to pretrained generator')
-#     args = vars(ap.parse_args())
+    ap.add_argument('-g', '--generator', required=False, help='path to pretrained generator')
+    ap.add_argument('-e', '--epochs', required=True, type=int, default=100, help='number of epochs')
+    args = vars(ap.parse_args())
 
     coco_path = untar_data(URLs.COCO_SAMPLE)
     coco_path = str(coco_path) + "/train_sample"
@@ -44,14 +46,18 @@ if __name__ == '__main__':
 
     train_dl = make_dataloaders(paths=train_paths, split='train')
     val_dl = make_dataloaders(paths=val_paths, split='val')
-    
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    net_G = build_res_unet(n_input=1, n_output=2, size=256)
-    net_G.load_state_dict(torch.load(args['generator'], map_location=device))
+
+    net_G = None
+    if args['generator']:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        net_G = build_res_unet(n_input=1, n_output=2, size=256)
+        net_G.load_state_dict(torch.load(args['generator'], map_location=device))
         
     model = MainModel(net_G=net_G)
     
-    samples_dir = f'samples_{time.time()}'
+    samples_dir = f'samples_{datetime.datetime.now().strftime("%Y-%m-%d_%I:%M%p")}'
+    if os.path.exists(samples_dir):
+        os.rmdir(samples_dir)
     os.mkdir(samples_dir)
         
-    train_model(model, train_dl, 100, visualize_dir=samples_dir)
+    train_model(model, train_dl, args['epochs'], visualize_dir=samples_dir)
